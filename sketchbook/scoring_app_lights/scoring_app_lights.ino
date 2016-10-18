3cbea9e9b0025995254398f18f77e2aa2ba80072
Rob Mackie <rob@mackies.org>
2016-10-17 21:17:07 -0400

    Changes to be committed: 	modified:   scoring_app_lights.ino

--- sketchbook/scoring_app_lights/scoring_app_lights.ino
+++ sketchbook/scoring_app_lights/scoring_app_lights.ino
@@ -9,20 +9,38 @@ const int NUM_LEDS = 40;
 
 const int LED_POS[LAST_PIN] = {4, 14, 24, 34};
 
+const int ALERT_PIN = 7;
+
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, CONTROL_PIN, NEO_GRB + NEO_KHZ800);
 
 uint32_t on;
 uint32_t off;
+uint32_t alert;
+uint32_t red;
+
+enum  redflag_t {
+  PRE,
+  TRANS,
+  POST
+};
+
+int captured_state[] = {0, 0, 0, 0};
+
+redflag_t redflag = PRE;
 
 void setup() {
-    for (int ii = 0; ii < LAST_PIN; ii++) {
-       pinMode(INPUT_PIN[ii], INPUT_PULLUP);
-    }
-  // put your setup code here, to run once:
+
+  for (int ii = 0; ii < LAST_PIN; ii++) {
+    pinMode(INPUT_PIN[ii], INPUT_PULLUP);
+  }
+  pinMode(ALERT_PIN, INPUT_PULLUP);
   strip.begin();
+
   on = strip.Color(0, 255, 0);
+  alert = strip.Color(0, 0, 100);
   off = strip.Color(0, 0, 0);
+  red = strip.Color(255, 0, 0);
 }
 
 void loop() {
@@ -30,12 +48,45 @@ void loop() {
 
   for (int ii = 0; ii < LAST_PIN; ii++) {
     if (digitalRead(INPUT_PIN[ii])) {
-    strip.setPixelColor(LED_POS[ii], on);
+      if (redflag == TRANS) {
+        captured_state[ii] = 1;
+      }
+      if (redflag == PRE || redflag == TRANS) {
+        strip.setPixelColor(LED_POS[ii], on);
+      }
+      if (redflag == POST) {
+        if (!captured_state[ii]) {
+          strip.setPixelColor(LED_POS[ii], red);
+        } else {
+          strip.setPixelColor(LED_POS[ii], on);
+        }
+      }
     } else {
+      if (redflag == TRANS) {
+        captured_state[ii] = 0;
+      }
+      if (redflag == POST) {
+        captured_state[ii] = 0;
+      }
       strip.setPixelColor(LED_POS[ii], off);
     }
   }
-  strip.show();
-  delay(25);
 
+
+for (int jj = 0; jj < NUM_LEDS; jj++) {
+  if (jj == 4 || jj == 14  || jj == 24 || jj == 34) {
+    continue;
+  }
+  if (digitalRead(ALERT_PIN) == LOW) {
+    strip.setPixelColor(jj, alert);
+    redflag = TRANS;
+  } else {
+    strip.setPixelColor(jj, off);
+    if (redflag == TRANS) {
+      redflag = POST;
+    }
+  }
+}
+strip.show();
+delay(25);
 }
