c2c676b78af0d7accc2c173846030372670e6cf1
Rob Mackie <rob@mackies.org>
2016-10-20 03:31:45 -0400

    basic ability to start on command and see score  Changes to be committed: 	modified:   push_the_green_button_scoring.ino

--- sketchbook/push_the_green_button_scoring/push_the_green_button_scoring.ino
+++ sketchbook/push_the_green_button_scoring/push_the_green_button_scoring.ino
@@ -3,7 +3,7 @@
 //////////////////////
 // WiFi Definitions //
 //////////////////////
-const char WiFiAPPSK[] = "pyrotech2016";
+// const char WiFiAPPSK[] = "PT_FIELD1";
 
 /////////////////////
 // Pin Definitions //
@@ -27,6 +27,9 @@ const unsigned long int RESET_DEBOUNCE = 5000;
 
 unsigned long int last_level_reset = 0;
 
+int captured_score = 0;
+int endgame = 0;
+
 int score = 0;
 
 WiFiServer server(80);
@@ -49,24 +52,34 @@ void print_state()
   }
 }
 
+uint32_t now = 0;
+uint32_t run_time = 0;
+
+bool started = false;
+
 void loop()
 {
   int flag = 0;
   if (!start_time) {
     start_time = millis();
   }
-  uint32_t now = millis();
-  uint32_t run_time = now - start_time;
+  now = millis();
+  run_time = now - start_time;
   if (run_time > 120000 && run_time <= 125000) {
     digitalWrite(ALERT_PIN, LOW);
-    flag = 0;
-  } else if (run_time > 125000) {
-    digitalWrite(ALERT_PIN, HIGH);
     flag = 1;
+  } else if (run_time > 125000 && run_time < 150000) {
+    digitalWrite(ALERT_PIN, HIGH);
+    flag = 0;
+  } else if (run_time > 150000 && run_time < 151000) {
+    if (!endgame) {
+      captured_score = score;
+      endgame = 1;
+    }
   } else {
     flag = 0;
   }
-  //  print_state();
+  print_state();
   // check reset pin, if on, and not recently pushed, then set everything to new level
   // Serial.println(digitalRead(LEVEL_UP_PIN));
   int levelup_read;
@@ -100,34 +113,72 @@ void loop()
   WiFiClient client = server.available();
   if (client) {
     // Read the first line of the request
-    String req = client.readStringUntil('\r');
-    // Serial.println(req);
+    String request = client.readStringUntil('\r');
+
     client.flush();
 
     // Prepare the response. Start with the common header:
-    String s = "HTTP/1.0 200 OK\r\n";
-    s += "Content-Type: text/html\r\n\r\n";
-    s += "<!DOCTYPE HTML>\r\n<html>\r\n";
-    s += "<head><meta http-equiv=\"refresh\" content=\"1\"> <title>Field 1 Scoring Page </title> </head>";
+    String s;
 
-    s += String("<p><h1>");
-    s += "Score: ";
-    s += String(score);
-    s += String("</h1></p>");
-    s += String("<p> start time: ") + String(start_time) + String("</p>");
-    s += String("<p> now time: ") + String(now) + String("</p>");
-    s += String("<p> run time: ") + String(run_time) + String("</p>");
-    s += String("<p> flag: ") + String(flag) + String("</p>");
+    if (request.indexOf("score") != -1) {
+      // if first time retrieval then start clock and scoring
+      if (!started) {
+        started = true;
+        button_state[0] = false;
+        digitalWrite(LED_PIN[0], LOW);
+        button_state[1] = false;
+        digitalWrite(LED_PIN[1], LOW);
+        button_state[2] = false;
+        digitalWrite(LED_PIN[2], LOW);
+        button_state[3] = false;
+        digitalWrite(LED_PIN[3], LOW);
+        last_level_reset = 0;
+        level = LAST_LEVEL - 1;
+        score = 0;
+        start_time = millis();
+        run_time = 0;
+        flag = 0;
+        endgame = 0;
+        captured_score = 0;
+      }
+      
+      s = "HTTP/1.0 200 OK\r\n";
+      s += "Content-Type: text/html\r\n\r\n";
+      s += "<!DOCTYPE HTML>\r\n<html>\r\n";
+      s += "<head><meta http-equiv=\"refresh\" content=\"1\"> <title>Field 1 Scoring Page </title> </head>";
 
-    s += "<br>"; // Go to the next line.
-    s += "</html>\n";
+      s += String("<p><h1>");
+      s += "Score: ";
+      if (endgame) {
+        s += String(captured_score);
+      } else {
+        s += String(score);
+      }
+      s += String("</h1></p>");
+      s += String("<p> <h1> run time: ") + String(run_time/1000) + String("</h1></p>");
 
+      s += "<br>"; // Go to the next line.
+      s += "</html>\n";
+
+    } else {
+
+      s = "HTTP/1.0 200 OK\r\n";
+      s += "Content-Type: text/html\r\n\r\n";
+      s += "<!DOCTYPE HTML>\r\n<html>\r\n";
+      s += "<head> <title>Field 1 Scoring Page </title> </head>";
+      s += String("<table><tr><td>");
+      s += String("<a href=\"/score\"><button> <H1> Start Now !  </H1> </button></a>");
+      s += String("</td>");
+      s += String("</tr></table>");
+      s += "</html>\n";
+      
+    }
     // Send the response to the client
     client.print(s);
     client.flush();
-    // Serial.println("Client disonnected");
+    Serial.println("Client disonnected");
+    delay(1);
   }
-  delay(1);
 }
 
 void setupWiFi()
@@ -135,7 +186,7 @@ void setupWiFi()
   Serial.println("Set up WiFi");
   WiFi.mode(WIFI_AP);
 
-  char* AP_SSID = "PyroTech2016";
+  char* AP_SSID = "PT_FIELD1";
   char* AP_PW = "password";
 
   WiFi.softAP(AP_SSID, AP_PW);
@@ -145,7 +196,6 @@ void setupWiFi()
   Serial.print("AP IP address: ");
 
   Serial.println(myIP);
-
 }
 
 void initHardware()
